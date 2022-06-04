3002d9e5f83f45a470a871050f7c60451fd78fbe
muhfirdaus08 <muhfirdaus0805@gmail.com>
2022-06-04 12:54:28 +0800

    Membuat animasi load more dengan jquery

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// NAVBAR
+
 const navbar = document.getElementsByTagName("nav")[0];
 window.addEventListener("scroll", function () {
     console.log(this.window.scrollY);
@@ -7,3 +9,21 @@ window.addEventListener("scroll", function () {
         navbar.classList.replace("nav-color", "bg-transparent");
     }
 });
+
+// END NAVBAR
+
+// JQUERY LOADMORE
+$(document).ready(function () {
+    $(".artikel-card").slice(0, 6).show();
+    $("#nocontent").hide();
+    $("#loadmore").on("click", function (e) {
+        e.preventDefault();
+        $(".artikel-card:hidden").slice(0, 3).slideDown();
+        if ($(".artikel-card:hidden").length == 0) {
+            $("#loadmore").hide();
+            $("#nocontent").show();
+        }
+    });
+});
+
+// END JQUERY LOADMORE
