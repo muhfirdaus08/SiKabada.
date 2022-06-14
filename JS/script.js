2ec56f7188a2ffae2d67c95b47c082ff3a329408
muhfirdaus08 <muhfirdaus0805@gmail.com>
2022-06-15 01:12:11 +0800

    Membuat Alert Box

--- JS/script.js
+++ JS/script.js
@@ -29,12 +29,67 @@ $(document).ready(function () {
         }
     });
 
-    $("#sembunyikan").click(function() {
+    $("#sembunyikan").click(function () {
         $("#sembunyikan").hide();
         $("#loadmore").show();
         $("#nocontent").hide();
-        $(".artikel-card").slice(2,8).hide();
+        $(".artikel-card").slice(2, 8).hide();
     });
 });
 
 // END JQUERY LOADMORE
+
+// ALERT
+
+// ALERT WARNING
+
+function myAlertWarning() {
+    swal("Mohon Maaf", "Anda Harus Daftar/Masuk Terlebih Dahulu", "warning");
+}
+
+// ALERT MASUK
+
+function myAlertMasuk() {
+    swal("Berhasil Masuk", "Selamat Datang Kembali di Sikabada.", "success");
+}
+
+// ALERT DAFTAR
+
+function myAlertDaftar() {
+    swal("Berhasil Daftar", "Selamat Datang di Sikabada.", "success");
+}
+
+// ALERT PERTANYAAN
+
+function myAlertPertanyaan() {
+    swal("Berhasil", "Pertanyaan Berhasil Terkirim", "success");
+}
+// ALERT Kritik/Saran
+
+function myAlertKritikSaran() {
+    swal("Berhasil", "Kritik/Saran Berhasil Terkirim", "success");
+}
+
+// ALERT Kontak Pesan
+
+function myAlertPesan() {
+    swal("Berhasil", "Pesan Berhasil Terkirim", "success");
+}
+// ALERT Kosa Kata
+
+function myAlertKosakata() {
+    swal("Berhasil", "Kosakata Berhasil Ditambahkan", "success");
+}
+
+// ALERT Artikel
+
+function myAlertArtikel() {
+    swal("Berhasil", "Tulisan Artikel Anda Berhasil Terkirim", "success");
+}
+// ALERT Event
+
+function myAlertEvent() {
+    swal("Berhasil", "Tulisan Event Anda Berhasil Terkirim", "success");
+}
+
+// END ALERT
